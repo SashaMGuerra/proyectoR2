4c214f28e619efdb6361fdc8e732a135407f02f5
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-10-19 17:54:58 +0200

    19/10/2021
    Inicio del repositorio.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Índice Proyecto R2</title>
+        <link href="webroot/style/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+        echo 'Proyecto R2';
+        echo 'Cambios para commit 1.1'.
+        phpinfo();
+        ?>
+    </body>
+</html>
