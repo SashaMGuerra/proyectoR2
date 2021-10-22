ebca67079b72ce4eff0a72373847c27c603423b6
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-10-22 13:37:51 +0200

    22/10/2021
    .htaccess

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Índice home.php alternativo</title>
+    </head>
+    <body>
+        <?php
+        echo 'vuelve a casa por navidad';
+        ?>
+        <ul>
+            <li><a href="ejercicio/colors2.php">Ejecutar</a></li>
+            <li><a href="ejercicio/factorial.php">Factorial</a></li>
+        </ul>
+    </body>
+</html>
